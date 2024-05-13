1ecd33cabd6ca1a8e6a751ba760e9def3dd7fb45
arun5309 <arun5309@gmail.com>
2024-05-13 14:19:51 +0530

    Enabled option hardware.opengl.enable for hyprland

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -37,4 +37,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Enable openGL stuff as hyprland doesn't enable it
+  hardware.opengl.enable = true;
 }
