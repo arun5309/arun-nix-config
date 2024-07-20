095e428c5a2c5060219017e9bf58211adea8fdd1
arun5309 <arun5309@gmail.com>
2024-07-20 20:59:54 +0530

    Added paths for audio plugins

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -402,6 +402,12 @@
     # EDITOR = "emacs";
     PAGER = "bat";
     VISUAL = "bat";
+    # Audio plugin paths
+    DSSI_PATH   = "$HOME/.dssi:$HOME/.nix-profile/lib/dssi:/run/current-system/sw/lib/dssi";
+    LADSPA_PATH = "$HOME/.ladspa:$HOME/.nix-profile/lib/ladspa:/run/current-system/sw/lib/ladspa";
+    LV2_PATH    = "$HOME/.lv2:$HOME/.nix-profile/lib/lv2:/run/current-system/sw/lib/lv2";
+    LXVST_PATH  = "$HOME/.lxvst:$HOME/.nix-profile/lib/lxvst:/run/current-system/sw/lib/lxvst";
+    VST_PATH    = "$HOME/.vst:$HOME/.nix-profile/lib/vst:/run/current-system/sw/lib/vst";
   };
 
   # Let Home Manager install and manage itself.
