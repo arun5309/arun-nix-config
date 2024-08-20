f181103c70085a3c3858d985028d7ea15885fb5c
arun5309 <arun5309@gmail.com>
2024-08-20 07:46:51 +0530

    Temporarily downgrading to LLVM 18 (for related projects as well) as LLVM 19 is not yet released.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -217,15 +217,15 @@
     openconnect
     lksctp-tools
     # Explicitly versioned stuff
-    llvmPackages_19.clang-unwrapped
-    llvmPackages_19.stdenv
-    llvmPackages_19.bintools-unwrapped
-    llvmPackages_19.openmp
-    llvmPackages_19.libunwind
-    llvmPackages_19.libclc
-    llvmPackages_19.libcxx
-    lld_19
-    lldb_19
+    llvmPackages_18.clang-unwrapped
+    llvmPackages_18.stdenv
+    llvmPackages_18.bintools-unwrapped
+    llvmPackages_18.openmp
+    llvmPackages_18.libunwind
+    llvmPackages_18.libclc
+    llvmPackages_18.libcxx
+    lld_18
+    lldb_18
     boost185
     python313Full
     nodejs_22
