9b07b2b947c2da1ab1fb7c36d8ab54b67b67b963
arun5309 <arun5309@gmail.com>
2025-09-22 08:44:44 +0530

    Configured network manager to randomize MAC addresses

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -85,8 +85,17 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
-  networking.networkmanager.dns = "systemd-resolved";
+  networking.networkmanager = {
+    enable = true;
+    dns = "systemd-resolved";
+    ethernet = {
+      macAddress = "random";
+    };
+    wifi = {
+      scanRandMacAddress = true;
+      macAddress = "random";
+    };
+  };
 
   networking.resolvconf.useLocalResolver = true;
   networking.search = [ "netbird.cloud" ];
